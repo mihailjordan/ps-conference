f94fb3e0c5385b3791d02b7dd2a298bd5377c5c3
Jordan Ivanov <mihailjordanski@gmail.com>
2021-05-05 10:24:55 +0200

    Comment java persistence configuration

--- src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
+++ src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
@@ -8,14 +8,13 @@ import javax.sql.DataSource;
 
 @Configuration
 public class PersistenceConfiguration {
-
-    @Bean
-    public DataSource dataSource() {
-        DataSourceBuilder builder = DataSourceBuilder.create();
-        builder.url("jdbc:postgresql://localhost:5432/conference_app");
-        builder.username("postgres");
-        builder.password("postgres");
-        System.out.println("Custom datasource bean has been created");
-        return builder.build();
-    }
+//    @Bean 
+//    public DataSource dataSource() {
+//        DataSourceBuilder builder = DataSourceBuilder.create();
+//        builder.url("jdbc:postgresql://localhost:5432/conference_app");
+//        builder.username("postgres");
+//        builder.password("postgres");
+//        System.out.println("Custom datasource bean has been created");
+//        return builder.build();
+//    }
 }
